9b0f4a38558698a3e10cc4979f09ac180192b678
Yusuke Sangenya <sangenya@sancha-mac.local>
2016-09-22 21:13:51 +0900

    changed the way how to override DataFrame#plotting_library= method

--- lib/daru_plotting_plotly/dataframe.rb
+++ lib/daru_plotting_plotly/dataframe.rb
@@ -1,14 +1,20 @@
+module PlottingLibraryPlotly
+  def plotting_library= lib
+    case lib
+    when :gruff, :nyaplot, :plotly
+      @plotting_library = lib
+      extend Module.const_get(
+        "Daru::Plotting::DataFrame::#{lib.to_s.capitalize}Library"
+      )
+    else
+			raise ArgumentError, "Plotting library #{lib} not supported. "\
+				'Supported libraries are :nyaplot and :gruff and :plotly'
+		end
+  end
+end
+
 module Daru
   class DataFrame
-    def plotting_library= lib
-      if lib == :plotly
-        @plotting_library = lib
-        extend Module.const_get(
-          "Daru::Plotting::DataFrame::#{lib.to_s.capitalize}Library"
-        )
-      else
-        super lib
-      end
-    end
+    prepend PlottingLibraryPlotly
   end
 end
