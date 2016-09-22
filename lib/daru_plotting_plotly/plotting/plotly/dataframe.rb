d796dbefa90acc7a910435a01bc6dd62a09fdb1e
Yusuke Sangenya <sangenya@sancha-mac.local>
2016-09-22 23:39:45 +0900

    generating data hash more cleverly

--- lib/daru_plotting_plotly/plotting/plotly/dataframe.rb
+++ lib/daru_plotting_plotly/plotting/plotly/dataframe.rb
@@ -12,17 +12,11 @@ module Daru
           mode = opts[:mode] || :'lines+markers'
 
           x = self[opts[:x] || :x].to_a
-          ys = if !opts[:y].nil?
-                 ys_before_format = self[*Array(opts[:y])]
-                 if ys_before_format.is_a?(Daru::DataFrame)
-                   ys_before_format.to_a
-                 else # when Daru::Vector
-                   [ys_before_format.to_a]
-                 end
-               else
-                 [self[:y].to_a]
-               end
-          data = ys.map { |y| { x: x, y: y, type: type, mode: mode } }
+          data = self[*Array(opts[:y] || :y)].to_df.map do |vector|
+            {
+              x: x, y: vector.to_a, type: type, mode: mode, name: vector.name
+            }
+          end
           plot = Plot.new(data: data, layout: layout)
           plot.show
           yield plot if block_given?
