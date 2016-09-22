0c98bebb4da9a9cd45f50b0a39aa2d57f15c21d0
Yusuke Sangenya <sangenya@sancha-mac.local>
2016-09-22 15:01:32 +0900

    changed directory name

--- lib/daru/plotting/plotly.rb
+++
@@ -1,4 +0,0 @@
-require 'daru/dataframe.rb'
-require 'daru/plotting/plotly/category.rb'
-require 'daru/plotting/plotly/dataframe.rb'
-require 'daru/plotting/plotly/vector.rb'
---
+++ lib/daru_plotting_plotly/plotting/plotly.rb
@@ -0,0 +1,4 @@
+require 'daru_plotting_plotly/dataframe.rb'
+require 'daru_plotting_plotly/plotting/plotly/category.rb'
+require 'daru_plotting_plotly/plotting/plotly/dataframe.rb'
+require 'daru_plotting-plotly/plotting/plotly/vector.rb'
