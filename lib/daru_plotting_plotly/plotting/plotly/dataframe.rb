40c8fe1db35469f30f38116184f268265168eeb6
Yusuke Sangenya <sangenya@sancha-mac.local>
2016-09-24 22:20:15 +0900

    check plot type

--- lib/daru_plotting_plotly/plotting/plotly/dataframe.rb
+++ lib/daru_plotting_plotly/plotting/plotly/dataframe.rb
@@ -7,7 +7,7 @@ module Daru
         include ::Plotly
 
         def plot opts={}
-          type = opts[:type] || :scatter
+          type = extract_type opts[:type]
           layout = { width: (opts[:width] || 500), height: (opts[:height] || 500) }
           mode = (Array(opts[:mode]) || [:lines, :markers]).map(&:to_s).join('+')
 
@@ -17,10 +17,22 @@ module Daru
               x: x, y: vector.to_a, type: type, mode: mode, name: vector.name
             }
           end
+
           plot = Plot.new(data: data, layout: layout)
           plot.show
           yield plot if block_given?
           plot
+        end
+
+        def extract_type plot_type
+          case plot_type
+          when :scatter, :bar
+            plot_type
+          when nil
+            :scatter
+          else
+            raise ArgumentError, 'daru_plotting_plotly currently supports only :scatter and :bar as plot type.'
+          end
         end
       end
     end
