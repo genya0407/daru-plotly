0904917ecc7140282d2c163c9af022219ee8c5c4
Yusuke Sangenya <longinus.eva@gmail.com>
2017-01-11 23:48:07 +0900

    module function

--- lib/daru_plotly.rb
+++ lib/daru_plotly.rb
@@ -4,7 +4,7 @@ require 'initializer/vector'
 require 'daru'
 
 module Daru::Plotly::Initializer
-  def self.plot(data, opts)
+  def plot(data, opts)
     if data.is_a? Daru::DataFrame
       Daru::Plotly::Initializer::DataFrame.plot(data, opts)
     elsif data.is_a? Daru::Vector
@@ -13,4 +13,6 @@ module Daru::Plotly::Initializer
       raise ArgumentError, 'first argument should be Daru::DataFrame or Daru::Vector.'
     end
   end
+
+  module_function :plot
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'daru'
-require 'plotly'
+require 'daru_plotly'
 require 'rbplotly'
